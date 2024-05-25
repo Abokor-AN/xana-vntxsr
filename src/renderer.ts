24893906bad14e27044f4115072b06ed4c6bf847
Abokor-AN <abokor.ahmed.kadar.nour@outlook.com>
2024-05-25 14:49:33 +0300

    Install vueapexcharts

--- src/renderer.ts
+++ src/renderer.ts
@@ -34,5 +34,11 @@ import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import i18n from "./lib/i18n";
+import VueApexCharts from "vue3-apexcharts";
 
-createApp(App).use(createPinia()).use(router).use(i18n).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(i18n)
+  .use(VueApexCharts)
+  .mount("#app");
